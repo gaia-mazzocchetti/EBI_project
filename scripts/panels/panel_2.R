7dcf3d559ba997e92c076da4da4adf53918f13cf
gaia-mazzocchetti <gaia.mazzocchetti95@gmail.com>
2026-03-16 15:04:08 +0000

    figure 2

--- scripts/panels/panel_2.R
+++ scripts/panels/panel_2.R
@@ -31,7 +31,7 @@ kras_plot <- ggplot(
   geom_col(width = 0.8) +
   scale_fill_gradientn(colors = embl_palette) +
   labs(
-    title = "Pathway enrichment in KRAS-mutated samples",
+    title = "Pathways of KRAS-mutated samples",
     x = expression(-log[10](p-value)),
     y = NULL,
     fill = expression(-log[10](p-value))
@@ -53,7 +53,7 @@ hras_plot <- ggplot(
   geom_col(width = 0.8) +
   scale_fill_gradientn(colors = embl_palette) +
   labs(
-    title = "Pathway enrichment in HRAS-mutated samples",
+    title = "Pathways of HRAS-mutated samples",
     x = expression(-log[10](p-value)),
     y = NULL,
     fill = expression(-log[10](p-value))
@@ -75,7 +75,7 @@ nras_plot <- ggplot(
   geom_col(width = 0.8) +
   scale_fill_gradientn(colors = embl_palette) +
   labs(
-    title = "Pathway enrichment in NRAS-mutated samples",
+    title = "Pathways of NRAS-mutated samples",
     x = expression(-log[10](p-value)),
     y = NULL,
     fill = expression(-log[10](p-value))
@@ -87,13 +87,13 @@ nras_plot <- ggplot(
     legend.position = "right"
   )
 
-ggpubr::ggarrange(kras_plot, hras_plot, nras_plot, labels = c("A", "B", "C"))
+ ggpubr::ggarrange(kras_plot, hras_plot, nras_plot, nrow = 1)
+print(panel_ptw)
 ggsave("../../plot/Figure_pathway_enrichment.pdf",
-       width = 20, height = 15)
+       width = 30, height = 10)
 
 
 
-setwd("~/Desktop/paper_analysis/paper_repository")
 deg_kras <- read.csv("output/DEG/deg_kras.csv")
 deg_hras <- read.csv("output/DEG/deg_hras.csv")
 deg_nras <- read.csv("output/DEG/deg_nras.csv")
@@ -101,6 +101,7 @@ deg_nras <- read.csv("output/DEG/deg_nras.csv")
 plot_db <- data.frame(Paralog = factor(c("KRAS", "HRAS", "NRAS"), levels = c("KRAS","NRAS", "HRAS")),
                       degs = c(nrow(deg_kras), nrow(deg_hras), nrow(deg_nras)))
 library(ggplot2)
+library(ggrepel)
 
 ggplot(plot_db, aes(Paralog, degs, fill= Paralog)) + 
   geom_bar(stat ="identity")+
@@ -121,3 +122,78 @@ head(deg_nras, n=10)
 deg_hras <- deg_hras %>% mutate(dir = ifelse(logFC >= 0.5, "up", ifelse(logFC <= -0.5, "down", "norm"))) %>% arrange(adj.P.Val, abs(logFC))
 table(deg_hras$dir)
 head(deg_hras, n=10)
+
+
+volcano_plot <- function(df, title, highlight_genes){
+  
+  df <- df %>%
+    mutate(
+      significance = case_when(
+        adj.P.Val < 0.05 & logFC > 0 ~ "Up",
+        adj.P.Val < 0.05 & logFC < 0 ~ "Down",
+        TRUE ~ "NS"
+      ),
+      label = ifelse(X %in% highlight_genes, X, NA)
+    )
+  
+  ggplot(df, aes(x = logFC, y = -log10(adj.P.Val))) +
+    
+    geom_point(aes(color = significance), size = 2, alpha = 0.8) +
+    
+    scale_color_manual(values = c(
+      "Up" = "#2E8B57",   # verde
+      "Down" = "#7A3E9D", # viola
+      "NS" = "grey80"
+    )) +
+    
+    geom_text_repel(
+      aes(label = label),
+      size = 5,
+      max.overlaps = 20
+    ) +
+    
+    labs(
+      title = title,
+      x = "log2 Fold Change",
+      y = "-log10 adjusted p-value"
+    ) +
+    
+    theme_classic() +
+    geom_vline(xintercept=c(-0.5,0.5), linetype="dashed", colour="grey60") +
+    geom_hline(yintercept=-log10(0.05), linetype="dashed", colour="grey60")+
+    
+    theme(
+      legend.position = "none",
+      plot.title = element_text(face = "bold", size = 12)
+    )
+}
+
+kras_genes <- c(
+  "MUC5AC","MUC2","MUC5B","TFF1", "REG4", "DKK4", "KRT7",
+  "DUSP4", "DUSP6","LEFTY1","RP11-350J20.12", "SLC26A3"
+)
+
+nras_genes <- c(
+  "DEFA4","AZU1","LY6G6D","AC104135.4",
+  "RP11-386M24.8","RP11-366L20.2", "SLC26A3"
+)
+
+hras_genes <- c(
+  "DPPA5","IFITM5","KIR3DX1","LIPK",
+  "AC005725.1","RP5-991C6.2","LINC00676", "RP11-59D5__B.2",
+  "AC006262.10", "LINC00704", "RP11-259O2.1"
+)
+
+
+p_kras <- volcano_plot(deg_kras, "KRAS DEGs", kras_genes)
+p_nras <- volcano_plot(deg_nras, "NRAS DEGs", nras_genes)
+p_hras <- volcano_plot(deg_hras, "HRAS DEGs", hras_genes)
+
+library(patchwork)
+
+final_plot <- (p_kras | p_nras | p_hras) / (kras_plot |hras_plot| nras_plot) +
+  plot_layout(heights = c(1, 1))
+
+
+final_plot + plot_annotation(tag_levels = "A")
+
